262331bc5fdfcac13e79473869823dd6a7c7aef4
Dmitry Mukhliada <d.mukhliada@softteco.com>
2021-02-20 19:40:57 +0300

    Fix amount range on Deposit creation Dto

--- PIRIS-labs/DTOs/Deposit/CreateDepositDto.cs
+++ PIRIS-labs/DTOs/Deposit/CreateDepositDto.cs
@@ -6,7 +6,7 @@ namespace PIRIS_labs.DTOs.Deposit
   public class CreateDepositDto
   {
     [Required]
-    [Range(0, 99999999999999, ErrorMessage = "Amount cant be negative")]
+    [Range(100, 99999999999999, ErrorMessage = "Amount can be in [100, 99999999999999]")]
     public decimal Amount { get; set; }
 
     [Required]
