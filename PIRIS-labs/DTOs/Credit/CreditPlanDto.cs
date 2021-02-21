7e903723534c84d1a5ede18617794926d4b17205
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-02-21 19:42:09 +0300

    Add creation of Credit plans

--- PIRIS-labs/DTOs/Credit/CreditPlanDto.cs
+++ PIRIS-labs/DTOs/Credit/CreditPlanDto.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PIRIS_labs.DTOs.Credit
 {
   public class CreditPlanDto
   {
     public Guid ID { get; set; }
+
+    [Required]
+    [MinLength(5)]
     public string Name { get; set; }
+
+    [Required]
+    [Range(12, 480, ErrorMessage = "Month period should be in [12, 480]")]
     public int MonthPeriod { get; set; }
+
+    [Required]
+    [Range(1, 5000, ErrorMessage = "Percent should be in [1, 5000]")]
     public decimal Percent { get; set; }
+
+    [Range(1, 100_000_000_000, ErrorMessage = "Min amount should be in [1, 100_000_000_000] or NULL")]
     public decimal? MinAmount { get; set; }
     public bool Anuity { get; set; }
   }
--- PIRIS-labs/Services/CreditPlansService.cs
+++ PIRIS-labs/Services/CreditPlansService.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using PIRIS_labs.Data;
+using PIRIS_labs.Data.Entities;
+using PIRIS_labs.DTOs;
 using PIRIS_labs.DTOs.Credit;
 
 namespace PIRIS_labs.Services
@@ -23,6 +25,17 @@ namespace PIRIS_labs.Services
       var creditPlans = await _unitOfWork.CreditPlans.GetCreditPlansOrderedAsync();
 
       return creditPlans.Select(_mapper.Map<CreditPlanDto>).ToList();
+    }
+
+    public async Task<ResultDto> CreateCreditPlanAsync(CreditPlanDto creditPlanDto)
+    {
+      var creditPlan = _mapper.Map<CreditPlan>(creditPlanDto);
+
+      _unitOfWork.CreditPlans.Add(creditPlan);
+
+      await _unitOfWork.SaveAsync();
+
+      return new ResultDto { Success = true };
     }
   }
 }
